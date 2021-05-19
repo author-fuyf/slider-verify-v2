9629647eed7c084b1c078e5205ad9a63e262d5e2
fuyf <fuyunfeng@gds-services.com>
2021-05-19 11:51:36 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,8 @@
-import toast from './Toast/index';
-import Button from './Button/index.vue';
 import SliderVerify from './SliderVerify/index.vue';
 
-// 准备好 install 方法 给 Vue.use() 使用
 const install = function (Vue) {
   if (install.installed) return;
   install.installed = true;
-
-  // 将包装好的 toast 挂到Vue的原型上，作为 Vue 实例上的方法
-  // Vue.prototype.$toast = toast;
-  // Vue.prototype.$test = 'tets';
-
-  // Vue.component(Button.name, Button)
   Vue.component(SliderVerify.name, SliderVerify)
 }
 
@@ -19,7 +10,6 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 
-// 默认导出 install
 export default {
   install,
 };
